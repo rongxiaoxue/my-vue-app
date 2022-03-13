355302a0929ae266c29d5d806db88572b16d0912
rong xiaoxue <shifouorzhenjia@gmail.com>
2022-03-13 16:33:45 +0800

    vite-vue-router 完成版本

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(
   {
   plugins: [vue()],
-  alias: {
-    'vue': 'vue/dist/vue.esm-bundler.js' // 定义vue的别名，如果使用其他的插件，可能会用到别名
+  resolve: {
+    alias:{
+      'vue': 'vue/dist/vue.esm-bundler.js' // 定义vue的别名，如果使用其他的插件，可能会用到别名
+  }
 },
 })
